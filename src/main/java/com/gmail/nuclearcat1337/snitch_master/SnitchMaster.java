ccd2c72be7fecb04a8db6fee027a79fea0c677ba
Gjum <code.gjum@gmail.com>
2016-11-21 03:48:12 +0100

    update version to 1.0.5

--- src/main/java/com/gmail/nuclearcat1337/snitch_master/SnitchMaster.java
+++ src/main/java/com/gmail/nuclearcat1337/snitch_master/SnitchMaster.java
@@ -31,7 +31,7 @@ public class SnitchMaster// implements SnitchMasterAPI
 {
     public static final String MODID = "snitchmaster";
     public static final String MODNAME = "Snitch Master";
-    public static final String MODVERSION = "1.0.4";
+    public static final String MODVERSION = "1.0.5";
 
     private static final Minecraft mc = Minecraft.getMinecraft();
     /**
